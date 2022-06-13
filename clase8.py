d2568e06b9dc161aef0fb763a0c6a5a68ee58a58
carlafaes <carlafaes18@hotmail.com>
2022-06-13 01:15:42 -0300

    while

---
+++ clase8.py
@@ -0,0 +1,15 @@
+#ciclo while 
+contador = 0
+while contador < 3:
+    print('Ejecutamos neustro ciclo while ',contador)
+    contador += 1
+else:
+    print('Fin del ciclo while')
+
+
+#imprimir numeros del 0 al 5 con el cilo while
+maximo = 5
+contador = 0
+while contador <= maximo:
+    print(contador)
+    contador += 1
