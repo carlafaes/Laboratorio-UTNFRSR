4260cf35e3eba683ba9aec648d92a06bd83d5f96
carlafaes <carlafaes18@hotmail.com>
2022-06-13 01:31:13 -0300

    m

--- clase8.py
+++ clase8.py
@@ -13,3 +13,19 @@ contador = 0
 while contador <= maximo:
     print(contador)
     contador += 1
+
+#imprimir numeros del 5 al 0 con el cilo while
+minimo = 1
+contador = 5
+while contador >= minimo:
+    print(contador)
+    contador -= 1
+
+
+#Ciclo for
+cadena = 'Hello'
+for letra in cadena:
+    print(letra)
+else:
+    print('Fin dek ciclo for')
+
