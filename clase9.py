8f3f2cc7e31ca1e1a0e9cf1010460192a5fd7045
carlafaes <carlafaes18@hotmail.com>
2022-06-27 13:55:30 -0300

    ejercicio clase 9

--- clase9.py
+++ clase9.py
@@ -1,12 +1,24 @@
 ##Diseñar un programa que ingresado un año, nos devuelva por consola si es un año bisiesto o no, repetir la accion hasta que el usuario ingrese un año que no sea bisiesto.
 
-num=0
-opcion=0
+# num=0
+# opcion=0
 
-while opcion != 1:
-    num=int(input("Ingrese un año: "))
-    if( num%4 == 0 and num%100 != 0 or num%400 == 0 ):
-        print(f"El año {num} es bisiesto")
-    else:
-        print(f"El año {num} no es bisiesto")
-    opcion=int(input("Desea ingresar otro año? (0=yes/1=no): "))
+# while opcion != 1:
+#     num=int(input("Ingrese un año: "))
+#     if( num%4 == 0 and num%100 != 0 or num%400 == 0 ):
+#         print(f"El año {num} es bisiesto")
+#     else:
+#         print(f"El año {num} no es bisiesto")
+#     opcion=int(input("Desea ingresar otro año? (0=yes/1=no): "))
+
+
+
+##Calcular la suma de "N" primeros numeros
+
+N=int(input("Digite la cantidad de numeros: "))
+suma=0
+while N > 0:
+    num=int(input("Digite el numero: "))
+    suma+=num
+    N-=1
+print(f"La suma de los numeros es: {suma}")
