6535dfcc72d69e665c3281a405b708f030413509
carlafaes <carlafaes18@hotmail.com>
2022-08-29 13:17:42 -0300

    primer ejercicio

---
+++ laboratorioII/clase1.py
@@ -0,0 +1,6 @@
+#lista: Ariel,Liliana,Natalia,Osvaldo
+
+nombres= ['Naty','Osvaldo','Lily','Ariel']
+print(nombres)##imprime toda la lista
+print(nombres[0])##imprime el primer elemento
+print(nombres[-1])##imprime ultimo elemento
