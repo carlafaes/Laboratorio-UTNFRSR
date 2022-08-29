17e0117346713b1b790b9f052a9f36aaae7b0dbb
carlafaes <carlafaes18@hotmail.com>
2022-08-29 14:56:59 -0300

    tuplas

--- laboratorioII/clase1/clase1.py
+++ laboratorioII/clase1/clase1.py
@@ -23,17 +23,44 @@ print(nombres)
 # insertar un elemento en un indice especifico de la lista
 nombres.insert(2, 'Juan')
 print(nombres)
-#eliminar el ultimo elemento
+# eliminar el ultimo elemento
 nombres.pop()
 print(nombres)
-#eliminar un elemento de un indice especifico
+# eliminar un elemento de un indice especifico
 del nombres[2]
 print(nombres)
-#eliminar o limpiar todos los elementos de la lista
+# eliminar o limpiar todos los elementos de la lista
 nombres.clear()
 print(nombres)
-#eliminar la lista
+# eliminar la lista
 del nombres
-print(nombres)
+# print(nombres)
 
+# tuplas
+# definimos una tupla
+cocina = ('cuchara', 'cuchillo', 'tenedor')
+print(cocina)
+print(len(cocina))
 
+# acceder a un elemento de la tupla
+print(cocina[0])
+# acceder a un rango
+print(cocina[0:1])
+# sale en consola ('cuchara',)
+# porque necesita una coma para ser considerado una tupla, de otra manera se consideraria un string
+
+# recorremos los elementos de la tupla
+for cocinar in cocina:
+    print(cocinar)
+    # en lugar de q imprima un salto de linea luego del elemento, imprime un espacio
+    print(cocinar, end=' ')
+
+# como modificar una tupla
+# se debe hacer un conversion de tupla a lista, modficacion, y nuevamente de lista a tupla,(de todas maneras, no es buena practica el modificar una tupla)
+cocinaModificada = list(cocina)
+cocinaModificada[0] = 'Plato'
+cocina = tuple(cocinaModificada)
+print(cocina)
+
+#para eliminar la tupla
+del cocina
