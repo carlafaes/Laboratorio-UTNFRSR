4818799c20b0e4e0e2a6d2f5ff6531f110067b87
carlafaes <carlafaes18@hotmail.com>
2022-09-21 15:13:22 -0300

    clase 5 ejercicios

---
+++ laboratorioII/clase5/Ejercicio1.py
@@ -0,0 +1,14 @@
+##Ejercicio 4: Sumar numeros pares dentro de un rango. Hacer un programa para sumar numeros pares dentrp de un rango, por ejemplo:
+## -suma de numeros pares del 2 al  30
+## -suma=240
+
+from re import I
+
+
+a= int(input("Digite de donde va a acomenzar la suma: "))
+b= int(input("Digite hasta donde quiere llegar a sumar: "))
+suma =0
+for i in range(a,b + 1):
+    if i % 2 == 0: ## Esto es si el numero es par
+        suma += i
+print(f"\nLa suma de numeros pares dentro del rango es: {suma}")
